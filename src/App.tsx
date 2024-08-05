0fac6085a1c49e67e35859199a23e2152385499c
Bohari S. Ambulo <bohariambulo17@gmail.com>
2024-08-05 23:54:37 +0800

    feat: added smooth scrolling to each pages when nav button clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,47 @@
+import { useRef } from "react";
 import { Navbar } from "./components/navbar/Navbar";
 import { useSelector } from "react-redux";
 import { RootState } from "./lib/store";
 import Home from "./pages/home/Home";
+import About from "./pages/about/About";
 
 function App() {
   const isDarkMode = useSelector(
     (state: RootState) => state.statusInfo.is_dark_mode
   );
+  const homeRef = useRef<HTMLDivElement | null>(null);
+  const aboutRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollToHome = () => {
+    if (homeRef.current) {
+      setTimeout(() => {
+        homeRef.current?.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    }
+  };
+
+  const scrollToAbout = () => {
+    if (aboutRef.current) {
+      setTimeout(() => {
+        aboutRef.current?.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    }
+  };
+
   return (
     <div
-      className={`w-screen h-screen ${
+      className={`w-screen h-auto ${
         isDarkMode ? "bg-darkmode-bgcolor" : "bg-white"
       }`}
     >
-      <Navbar />
-      <Home />
+      <Navbar onClickHome={scrollToHome} onClickAbout={scrollToAbout} />
+      <div ref={homeRef}>
+        <Home />
+      </div>
+
+      <div ref={aboutRef}>
+        <About />
+      </div>
     </div>
   );
 }
